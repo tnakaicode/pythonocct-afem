ad948b3c3faa8a87e512ceb4c3ed2c5db95a7a1b
tnakaicode <tnakaicode@gmail.com>
2021-06-27 04:04:33 +0900

    setup

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 from setuptools import setup
 
+# pip install wxPython
+
 setup(
     name='afem',
     version='1.1.0',
